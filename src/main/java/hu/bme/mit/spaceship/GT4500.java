2d8de76e2b45357412450381c972e7c811d93bfe
tivaldo <strobl.dani@gmail.com>
2024-05-17 01:14:04 +0200

    mock not finished

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -10,9 +10,11 @@ public class GT4500 implements SpaceShip {
 
   private boolean wasPrimaryFiredLast = false;
 
-  public GT4500() {
-    this.primaryTorpedoStore = new TorpedoStore(10);
-    this.secondaryTorpedoStore = new TorpedoStore(10);
+  public GT4500(TorpedoStore first, TorpedoStore second) {
+    //this.primaryTorpedoStore = new TorpedoStore(10);
+    //this.secondaryTorpedoStore = new TorpedoStore(10);
+    this.primaryTorpedoStore = first;
+    this.secondaryTorpedoStore = second;
   }
 
   public boolean fireLaser(FiringMode firingMode) {
--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,31 +9,79 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockTS1;
+  private TorpedoStore mockTS2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    this.mockTS1 = mock(TorpedoStore.class);
+    this.mockTS2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockTS1, mockTS2);
   }
 
   @Test
-  public void fireTorpedo_Single_Success(){
+  public void TestCase1(){
     // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
     assertEquals(true, result);
   }
 
   @Test
-  public void fireTorpedo_All_Success(){
+  public void TestCase2(){
     // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(1)).fire(1);
+    assertEquals(true, result);
+    assertEquals(true, result2);
+  }
+
+  @Test
+  public void TestCase3(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(0)).fire(1);
+    verify(mockTS2, times(1)).fire(1);
+    assertEquals(true, result);
+  }
+
+  
+
+
+
+  @Test
+  public void TestCase5(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(1)).fire(1);
     assertEquals(true, result);
   }
 
