234fe50d068004fc00eafd3f320733f9c74d0783
tivaldo <128145073+tivaldo@users.noreply.github.com>
2024-05-17 11:32:31 +0200

    Update GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -56,6 +56,7 @@ public class GT4500Test {
     // Arrange
     when(mockTS1.fire(1)).thenReturn(true);
     when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS1.isEmpty()).thenReturn(true);
     
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
@@ -66,9 +67,22 @@ public class GT4500Test {
     assertEquals(true, result);
   }
 
-  
+  @Test
+  public void TestCase4(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
 
-
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(false, result);
+    assertEquals(false, result2);
+  }
 
   @Test
   public void TestCase5(){
@@ -84,5 +98,137 @@ public class GT4500Test {
     verify(mockTS2, times(1)).fire(1);
     assertEquals(true, result);
   }
+
+  @Test
+  public void TestCase6(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void TestCoverage1(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(2)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(true, result);
+    assertEquals(true, result2);
+  }
+
+  @Test
+  public void TestCoverage2(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(false);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result3 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(1)).fire(1);
+    assertEquals(true, result);
+    assertEquals(false, result2);
+    assertEquals(false, result3);
+  }
+
+  @Test
+  public void TestCoverage3(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(0)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(false, result);
+    assertEquals(false, result2);
+  }
+
+  @Test
+  public void TestCoverage4(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    when(mockTS1.isEmpty()).thenReturn(true);
+
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+    
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(true, result);
+    assertEquals(false, result2);
+  }
+
+  @Test
+  public void TestCoverage5(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    
+    // Assert
+    verify(mockTS1, times(0)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void TestCoverage6(){
+    // Arrange
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.fire(1)).thenReturn(true);
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.valueOf("ALL"));
+    
+    // Assert
+    verify(mockTS1, times(0)).fire(1);
+    verify(mockTS2, times(0)).fire(1);
+    assertEquals(false, result);
+  }
+
+
+
+
+
+
 
 }
